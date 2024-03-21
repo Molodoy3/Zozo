3128cf5d6945cdd2f073feb5396ff46cd617745a
Molodoy3 <artem.nikonov.2005@bk.ru>
2024-03-21 20:43:19 +0500

    Добавьте файлы проекта.

---
+++ wpf/Controllers/UsersController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf.Model;
+using System.Security.Cryptography;
+
+namespace wpf.Controllers
+{
+    internal class UsersController
+    {
+        Core db = new Core();
+        public string CheckAuth(string login, string password)
+        {
+            string textError = "Данные введены не верно";
+            string result = "";
+            if (string.IsNullOrEmpty(password))
+                throw new Exception("Вы не ввели пароль");
+            if (string.IsNullOrEmpty(login))
+                throw new Exception("Вы не ввели логин");
+
+            var user = db.context.Users.FirstOrDefault(x => x.Login == login);
+            if (user != null)
+            {
+                Console.WriteLine(getHash(password), user.Password);
+                if (getHash(password) == user.Password)
+                {
+                    result = "client";
+                }
+                else
+                    throw new Exception(textError);
+            }
+            else
+            {
+                var specialist = db.context.Specialistes.FirstOrDefault(x => x.Login == login);
+                if (specialist != null)
+                {
+                    if (getHash(password) == specialist.Password)
+                    {
+                        switch (specialist.Status)
+                        {
+                            case "specialist":
+                                result = "specialist";
+                                break;
+                            case "manager":
+                                result = "manager";
+                                break;
+                            case "admin":
+                                result = "admin";
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                } else
+                   throw new Exception("f");
+            }
+
+            return result;
+        }
+        public void SetLocalSettings(string status)
+        {
+            //Properties.Settings.Default.idRole = user.IdRole;
+            //Properties.Settings.Default.idClient = user.IdUser;
+            //Properties.Settings.Default.Save();
+        }
+        private string getHash(string password)
+        {
+            MD5 md5 = MD5.Create();
+            byte[] b = Encoding.ASCII.GetBytes(password);
+            byte[] hash = md5.ComputeHash(b);
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var a in hash)
+                stringBuilder.Append(a.ToString("X2"));
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ wpf/Views/Pages/AuthPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using wpf.Controllers;
+using wpf.Model;
+using wpf.Views.Pages.Client;
+
+namespace wpf.Views.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        Core db = new Core();
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthButtonClick(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordTextBox.Password;
+
+            try
+            {
+
+                UsersController usersController = new UsersController();
+
+                string userStatus = usersController.CheckAuth(login, password);
+                if (userStatus != "")
+                {
+                    usersController.SetLocalSettings(userStatus);
+                    this.NavigationService.Navigate(new MainPage());
+                }
+                //if (userStatus == "client")
+                //    this.NavigationService.Navigate(new ClientPersonalCabinet());
+                //else if (usersController.CheckAuth(login, password) == "specialist")
+                //    this.NavigationService.Navigate(new ClientPersonalCabinet());
+                //else if (usersController.CheckAuth(login, password) == "manager")
+                //    this.NavigationService.Navigate(new ClientPersonalCabinet());
+                //else if (usersController.CheckAuth(login, password) == "admin")
+                //    this.NavigationService.Navigate(new ClientPersonalCabinet());
+
+            } catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
